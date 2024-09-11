fad5df7ba61264f1c8d7398abe3a9b4145003423
Caxseii <pedro.schutz192@gmail.com>
2024-09-10 21:24:40 -0300

    Atividade UML: Pessoa + Endereco

--- Aula 05/Associações de Classes/associationProject/src/cadastroPessoa/Endereco.java
+++ Aula 05/Associações de Classes/associationProject/src/cadastroPessoa/Endereco.java
@@ -1,8 +1,37 @@
 package cadastroPessoa;
 
 public class Endereco {
-    /**
-     * DECLARAR ATRIBUTOS
-     */
+    private Integer cep;
+    private Integer numero;
+
+    public Integer getNumero() {
+        return numero;
+    }
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void Integer setNumero(Integer numero) {
+        if(numero != null && !numero.isEmpty()){
+            this.numero = numero;
+        }else{
+            System.out.println("Numero inválido");
+        }
+    }
+
+    public void Integer setCep(Integer cep) {
+        if(cep != null && !cep.isEmpty()){
+            this.cep = cep;
+        }else{
+            System.out.println("CEP inválido");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "cep='" + cep + '\'' +
+                ", numero='" + numero + '\'' +
+                '}';
+    }
 
 }
--- Aula 05/Associações de Classes/associationProject/src/cadastroPessoa/Pessoa.java
+++ Aula 05/Associações de Classes/associationProject/src/cadastroPessoa/Pessoa.java
@@ -8,13 +8,17 @@ public class Pessoa {
     private Integer idade;
     private Pessoa pai;
     private Pessoa mae;
-    private Endereco enderecoAtual;
     private List<Endereco> enderecos;
 
-    public Pessoa(String nome, Integer idade) {
+    public Pessoa(String nome, Integer idade, Endereco enderecoAtual) {
         this.nome = nome;
         this.idade = idade;
         this.enderecos = new ArrayList<>();
+        this.incluirIndereco(enderecoAtual);
+    }
+
+    public void incluirIndereco(Endereco enderecoAtual){
+        this.enderecos.add(enderecoAtual);
     }
 
     public String getNome() {
@@ -33,3 +37,13 @@ public class Pessoa {
         return enderecos;
     }
 }
+
+@Override
+public String toString() {
+    return "Pessoa{" +
+            "nome='" + nome + '\'' +
+            ", idade='" + idade + '\'' +
+            '}';
+}
+}
+
